87c352ed548ce090d3b875a94abad09d8ca2dc9c
Joe Thomas <1andonlyjopa45@gmail.com>
2023-02-09 16:08:08 -0500

    upgrade button works

--- Code/Game/test.py
+++ Code/Game/test.py
@@ -64,6 +64,9 @@ root.rowconfigure(1, weight = 1)
 #FUNCTIONS connected to Game class for in-game Buttons
 def create_energy():
     game.create_energy()
+
+def buy_upgrade1():
+    game.buy_upgrade_1()
     
 
 #CREATION of Labels that go onto Frames/Buttons
@@ -97,7 +100,8 @@ energyB = tk.Button(createLabel, text ="Energy", command = create_energy, font=(
 
 timeB = tk.Button(time, text = "Advance Time", font=('Terminal', 10))
 
-upgrade1 = tk.Button(upgrades, text = "Energy Production +1 \n Auto-Energy + 0.1/sec", font = ('Terminal', 10), height = 5, width = 50, state = DISABLED)
+upgrade1 = tk.Button(upgrades, text = "Energy Production +1 \n Auto-Energy + 0.1/sec", command = buy_upgrade1,
+     font = ('Terminal', 10), height = 5, width = 50, state = DISABLED)
 
 #PLACEMENT of Buttons in Frames
 energyB.place(in_ = createLabel, y = 10, x = 20)
@@ -107,13 +111,18 @@ timeB.place(in_ = time, x = 25, y = 5)
 upgrade1.place(in_ = upgrades, x = 35, y = 20)
 
 #If statement for upgrade button DOES NOT CURRENTLY WORK
-if game.energy == 10:
-        upgrade1.state = ACTIVE
+
 
 #Function that constantly updates game Labels
 def update_labels():
     game = Game() # get the instance of the game class
     energy.config(energy, text = "Energy: " + str(game.energy))
+    if game.energy >= 10:
+        upgrade1.config(upgrade1, state = ACTIVE)
+    elif game.energy < 10:
+        upgrade1.config(upgrade1, state = DISABLED)
+
+
     root.after(10, update_labels) # schedule the function to be called again after 1000ms
 
 #Call to continously run update_labels function
