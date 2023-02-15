09a61b915dc2a415fad89163e2bd2cf1b66fd3bb
JoeT00 <94569866+JoeT00@users.noreply.github.com>
2023-02-15 16:23:12 -0500

    got rid of commented thingys

--- Code/Game/test.py
+++ Code/Game/test.py
@@ -53,6 +53,7 @@ compCostLab = None
 # Upgrades
 upBut_GC = None # Gravitational Compression
 upBut_SS = None # Subatomic Synthesis
+
 # Automators
 autoBut_Comp = None # Compressor
 
@@ -62,15 +63,9 @@ timeline = Frame(root, relief = RAISED, bd = 5, bg = "white", height = 90, width
 
 lifeForms = Frame(root, relief= RAISED, bd = 5, bg = "purple", height = 40, width = 2000)
 
-# resourcesF = Frame(root, relief = RAISED, bd = 5, bg = "red", height = resourceSize, width = 325)
-
 visuals = Frame(root, relief = RAISED, bd = 5, bg = "yellow", height = 450, width = 450)
 
 createLabel = Frame(root, bg = "orange", height = 40, width = 100)
-
-# production = Frame(root, relief = RAISED, bd = 5, bg = "green", height = productionSize, width = 325)
-
-# upgrades = Frame (root, relief = RAISED, bd = 5, bg = "teal", height = upgradeSize, width = 503)
 
 time = Frame (root, relief = RAISED, bd = 5, bg = "pink", height = 40, width = 175)
 
@@ -79,15 +74,9 @@ timeline.place(x = 20, y = 0)
 
 lifeForms.place(x = 20, y = 90)
 
-# resourcesF.place(x = 20, y = resourcePOS)
-
 visuals.place(x = 900, y = resourcePOS)
 
 createLabel.place(x = 20, y = 135)
-
-# production.place(x = 20, y = resourcePOS + resourceSize + 5)
-
-# upgrades.place(x = 350, y = resourcePOS)
 
 time.place(x = 500, y = 135)
 
@@ -123,34 +112,17 @@ def increase_compression():
     
 
 #CREATION of Labels that go onto Frames/Buttons
-# energy = Label(resources, text = "Energy: " + str(game.energy), font = ('Terminal', 10))
-
 tlL = Label(timeline, text = "Timeline:", font = ('Terminal', 10))
 
 lifeL = Label(lifeForms, text = "Lifeforms: ", font = ("Terminal", 10))
 
 eraL = Label(visuals, text = "Era: ", font = ("Terminal", 10))
 
-# resourcesL = Label(resources, text = "Resources", font = ("Terminal", 10))
-
-# productionL = Label(production, text = "Production", font = ("Terminal", 10))
-
-# upgradesL = Label(upgrades, text = "Upgrades", font = ("Terminal", 10))
-
-#PLACEMENT of Labels within respected Frame/Button
-# energy.place(x = 0, y = 25)
-
 lifeL.place(x = 0, y = 0)
 
 tlL.place(x = 0, y = 0)
 
 eraL.place(x = 0, y = 0)
-
-# resourcesL.place(x = 90, y = 0)
-
-# productionL.place(x = 90, y = 0)
-
-# upgradesL.place(x = 200, y = 0)
 
 #CREATION of Buttons
 energyB = tk.Button(createLabel, text ="Energy", command = create_energy, font=('Terminal', 10))#, image = img)
@@ -161,19 +133,10 @@ neutronB = tk.Button()
 
 timeB = tk.Button(time, text = "Advance Time", font=('Terminal', 10), state = DISABLED)
 
-# upg_SiphonRadition = tk.Button(upgrades, text = GravitationalCompression().name + "\n" + GravitationalCompression().description + "\n" + GravitationalCompression().showCosts(),
-#     command = buy_SiphonRadiation, font = ('Terminal', 10), height = 5, width = 50, state = DISABLED)
-
-
-
-
 #PLACEMENT of Buttons in Frames
 energyB.place(in_ = createLabel, y = 10, x = 20)
 
 timeB.place(in_ = time, x = 25, y = 5)
-
-# upg_SiphonRadition.place(in_ = upgrades, x = 35, y = 20)
-
 
 # Checks milestone conditions, activating new game mechanics when achieved
 def check_milestones():
@@ -286,8 +249,6 @@ def update_labels():
     if game.resourcesFrame == True:
         global energyLab
         energyLab.config(energyLab, text = "Energy: " + str(round(game.energy)))
-    # if game.production == True:
-    #     None
 
     root.after(100, update_labels) # schedule the function to be called again after 1000ms
 
